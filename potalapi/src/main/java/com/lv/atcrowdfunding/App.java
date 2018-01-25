0f80582df9f4313b4bab64ca77adc83dd249b543
lvbingyu <1955244751@qq.com>
2018-01-25 15:03:19 +0800

    项目环境搭建第一版

---
+++ potalapi/src/main/java/com/lv/atcrowdfunding/App.java
@@ -0,0 +1,13 @@
+package com.lv.atcrowdfunding;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
